47f161749aa01226a47316ee702cfd94123a4dda
Malu202 <poehlmann.martin@gmail.com>
2018-02-18 00:19:50 +0100

    performance optimizations

--- komischerweise schneller/komischerweise_schneller.ino
+++ komischerweise schneller/komischerweise_schneller.ino
@@ -6,13 +6,11 @@ class outputCommand {
   private:
     //unsigned short int millisecondsSinceLastToggle = 0;
     unsigned short int lastToggleTime;
-    unsigned char pinNumber = LED_BUILTIN;
+    unsigned char pinNumber;
     boolean pinValue;
     unsigned char toggleTimeIndex;
     unsigned short int toggleTimes [MAX_TOGGLE_TIMES];
-    unsigned char toggleTimesSize=0;
-    
-    
+
   public:
     boolean empty = true;
     boolean initialized = false;
@@ -23,17 +21,16 @@ class outputCommand {
       empty = false;
       toggleTimeIndex = 0;
       unsigned short int zero = 0;
-      for (unsigned char i = 1; i < STELLEN_FUER_TOGGLE_TIMES; i++) {
+      for (unsigned char i = 1; i < MAX_TOGGLE_TIMES; i++) {
         toggleTimes[i] = zero;
       }
       lastToggleTime = millis();
       pinValue = initialValue;
       //Damit sofort zu beginn auf die initalValue getoggled wird
       toggleTimes[0] = 0;
-      toggleTimesSize = 1;
     }
     void toString() {
-      for (unsigned char i = 0; i < MAX_TOGGLE_TIMES; i++) {
+      for (unsigned char i = 0; i <= MAX_TOGGLE_TIMES; i++) {
         Serial.print(toggleTimes[i]);
         Serial.print(' ');
       }
@@ -42,9 +39,13 @@ class outputCommand {
       if (pinValue) Serial.println("true");
       else Serial.println("false");
     }
-    void addToggleTime(unsigned short int downloadingToggleTime) {      
-          toggleTimes[toggleTimesSize] = downloadingToggleTime;
-          toggleTimesSize++;      
+    void addToggleTime(unsigned short int toggleTime) {
+      for (unsigned char i = 1; i <= MAX_TOGGLE_TIMES; i++) {
+        if (toggleTimes[i] == 0) {
+          toggleTimes[i] = toggleTime;
+          break;
+        }
+      }
     }
     boolean update() {
       if (initialized) {
@@ -68,19 +69,14 @@ class outputCommand {
 outputCommand commands[MAX_PARALLEL_COMMANDS];
 
 void setup() {
-  //Serial.begin(230400); //E wird nicht mehr gesendet
-  //Serial.begin(57600);
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println('hi');
 }
 
 unsigned short j;
 outputCommand downloadingCommand;
-char downloadingToggleTime[STELLEN_FUER_TOGGLE_TIMES];
-unsigned char i = 0;
-
 void loop() {
-  long before = micros();
   for (j = 0; j < MAX_PARALLEL_COMMANDS; j++) {
     if (commands[j].empty) {
       break;
@@ -88,42 +84,47 @@ void loop() {
       commands[j].update();
     }
   }
-  //Serial.println(micros() - before);
-  
-  if (i == STELLEN_FUER_TOGGLE_TIMES) {      
-    downloadingCommand.addToggleTime(downloadingToggleTime[0] * 10000 + downloadingToggleTime[1] * 1000 + downloadingToggleTime [2] * 100 + downloadingToggleTime[3] * 10 + downloadingToggleTime[4]);
-    i=0;
-  } else if (Serial.available()) {
+  if (Serial.available()) {
+    long before = millis();
 
     char newChar = Serial.read();
     byte newByte = newChar;
 
     switch (newChar) {
       case 'H' :
-        //Serial.println('H');
+        Serial.println('H');
         downloadingCommand = outputCommand(true);
         break;
       case 'L' :
-        //Serial.println('L');
+        Serial.println('L');
         downloadingCommand = outputCommand(false);
         break;
       case 'E' :
-        //Serial.println('E');
-        downloadingCommand.initialized = true;
+        Serial.println('E');
         for (unsigned char k = 0; k < MAX_PARALLEL_COMMANDS; k++) {
-          if (commands[k].empty){
-            commands[k] = downloadingCommand;
-            break;
-          }
+          if (commands[k].empty) commands[k] = downloadingCommand;
+          break;
         }
         //downloadingCommand.toString();
         break;
       default :
-        //Serial.println("default");
-        downloadingToggleTime[i] = newChar - 48;
-        i++;          
+        Serial.println("default");
+        char toggleTime[STELLEN_FUER_TOGGLE_TIMES];
+        toggleTime[0] = newChar - 48;
+        unsigned char i = 1;
+        while (true) {
+          if (Serial.available()) {
+            toggleTime[i] = Serial.read() - 48;
+            i++;
+            if (i >= STELLEN_FUER_TOGGLE_TIMES) break;
+          }
+        }
+        unsigned short int toggleTimeAssembly = toggleTime[0] * 10000 + toggleTime[1] * 1000 + toggleTime [2] * 100 + toggleTime[3] * 10 + toggleTime[4];
+        downloadingCommand.addToggleTime(toggleTimeAssembly);
     }
-    //Serial.print("Looptime: ");
+    Serial.print("Looptime: ");
+    Serial.println(millis() - before);
+
   }
 }
 
